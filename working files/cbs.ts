94410fb9ee39b0161609e048c80818adbd1cb6f1
Andrew M <amess3309@gmail.com>
2024-04-22 12:12:15 -0500

    update cbs, move files back to folder

--- working files/cbs.ts
+++ working files/cbs.ts
@@ -1,13 +1,17 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const fs = require("fs");
 import { Browser } from "puppeteer";
 const dayjs = require("dayjs");
 
 const currentDate = dayjs().format("MM-DD-YYYY");
+puppeteer.use(StealthPlugin());
 
-let url = [
+const { executablePath } = require("puppeteer");
+
+let draftTypes = [
   {
-    url: "https://www.cbssports.com/fantasy/football/draft/averages/",
+    url: "https://www.cbssports.com/fantasy/football/draft/averages/both/h2h/all/",
     lable: "CBS Standard",
   },
   {
@@ -16,19 +20,24 @@ let url = [
   },
 ];
 
-const CBS_ADP = async (url: { url: string; lable: string }) => {
-  const browser: Browser = await puppeteer.launch();
+const CBS_ADP = async (draftTypes: { url: string; lable: string }) => {
+  const browser: Browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: false,
+    executablePath: executablePath(),
+  });
   const page = await browser.newPage();
-  await page.goto(url.url);
+  await page.goto(draftTypes.url, { waitUntil: "domcontentloaded" });
+  await page.waitForSelector("table > tbody > tr");
 
   const adpData = await page.evaluate(() => {
     const playerRows = Array.from(
-      document.querySelectorAll(".TableBase-bodyTr")
+      document.querySelectorAll("table > tbody > tr")
     );
     // map each row in the table
     const data = playerRows.map((player: any) => ({
       // player rank data saved as rank
-      rank: player.querySelector(" td:nth-child(1) ").innerText,
+      rank: player.querySelector("td:nth-child(1)").innerText,
       playerName: player.querySelector(
         "td:nth-child(2) > span.CellPlayerName--long > span > a"
       ).innerText,
@@ -41,11 +50,11 @@ const CBS_ADP = async (url: { url: string; lable: string }) => {
       // player team data saved as team
       team: player
         .querySelector(
-          " td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-team "
+          "td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-team"
         )
         .innerText.trim(),
       // player adp data saved as adp
-      adp: player.querySelector(" td:nth-child(4) ").innerText,
+      adp: player.querySelector("td:nth-child(4)").innerText,
     }));
     return data;
   });
@@ -53,7 +62,7 @@ const CBS_ADP = async (url: { url: string; lable: string }) => {
   console.log(adpData);
   await browser.close();
   fs.writeFileSync(
-    `${url.lable} ${currentDate}.json`,
+    `${draftTypes.lable} ${currentDate}.json`,
     JSON.stringify(adpData),
     (err: any) => {
       if (err) throw err;
@@ -62,4 +71,4 @@ const CBS_ADP = async (url: { url: string; lable: string }) => {
   );
 };
 
-url.forEach((url) => CBS_ADP(url));
+draftTypes.forEach((url) => CBS_ADP(url));
