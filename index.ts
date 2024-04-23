1906478e6b12e5954d6fb5f197d95de3c7e626e2
Andrew M <amess3309@gmail.com>
2024-04-23 11:52:58 -0500

    add env and gitignore files, start on cbs league settings

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ puppeteer.use(StealthPlugin());
 
 const { executablePath } = require("puppeteer");
 
-const url = "https://fantasy.espn.com/football/livedraftresults";
+const url = "https://www.cbssports.com/fantasy/football/";
 
 (async () => {
   const browser: Browser = await puppeteer.launch({
@@ -16,165 +16,202 @@ const url = "https://fantasy.espn.com/football/livedraftresults";
     executablePath: executablePath(),
   });
   const page = await browser.newPage();
-  await page.goto(url);
-
-  let players = new Array();
-  let isBtnDisabled = false;
-  let firstPlayer = "";
-
-  await page.waitForSelector("tbody > tr");
-
-  while (!isBtnDisabled) {
-    console.log("*** RUNNING SCRAPE ***");
-    console.log({ firstPlayer });
-
-    await page.waitForFunction(
-      (p) => {
-        const player = document.querySelector<HTMLDivElement>(
-          "tbody > tr > td:nth-child(2) > div"
-        )?.title;
-
-        if (!p || player !== p) {
-          console.log("*** NEW PAGE, SETTING PLAYER ***");
-          return true;
-        }
-
-        console.log("*** RETURNING FALSE ***");
-        return false;
-      },
-      { polling: 5000, timeout: 60000 },
-      firstPlayer
-    );
-
-    const first = await page.evaluate(() => {
-      return document.querySelector<HTMLElement>(
-        "tbody > tr > td:nth-child(2) > div"
-      )?.title;
-    });
-
-    firstPlayer = first ?? "";
-
-    console.log("*** GETTING PLAYER ROWS ***");
-
-    const playerRows = await page.$$(".Table__TBODY > tr");
-
-    for (const playerData of playerRows) {
-      let rank: any = "Null";
-      let playerName: any = "Null";
-      let position: any = "Null";
-      let team: any = "Null";
-      let adp: any = "Null";
-      let changeADP: any = "Null";
-      let auctionValue: any = "Null";
-      let auctionChange: any = "Null";
-
-      try {
-        rank = await page.evaluate(
-          (el: any) => el.querySelector("td:nth-child(1) > div").innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        playerName = await page.evaluate(
-          (el: any) =>
-            el.querySelector(
-              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__athlete.flex > span > a"
-            ).innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        position = await page.evaluate(
-          (el: any) =>
-            el.querySelector(
-              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerpos.ttu"
-            ).innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        team = await page.evaluate(
-          (el: any) =>
-            el.querySelector(
-              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerteam"
-            ).innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        adp = await page.evaluate(
-          (el: any) => el.querySelector("td:nth-child(3) > div").innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        changeADP = await page.evaluate(
-          (el: any) =>
-            el.querySelector("td:nth-child(4) > div > span").innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        auctionValue = await page.evaluate(
-          (el: any) => el.querySelector("td:nth-child(5) > div").innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      try {
-        auctionChange = await page.evaluate(
-          (el: any) => el.querySelector("td:nth-child(6) > div").innerText,
-          playerData
-        );
-      } catch (error) {}
-
-      if (rank !== "Null") {
-        players.push({
-          rank,
-          playerName,
-          position,
-          team,
-          adp,
-          changeADP,
-          auctionValue,
-          auctionChange,
-        });
-      }
-    }
-
-    console.log("*** FINDING NEXT BTN ***");
-
-    await page.waitForSelector(".Pagination__Button--next", {
-      visible: true,
-    });
-
-    const is_disabled =
-      (await page.$(".Pagination__Button--next.Button--disabled")) !== null;
-
-    isBtnDisabled = is_disabled;
-    console.log("*** DISABLED:", is_disabled, "***");
-
-    if (!is_disabled) {
-      console.log("*** CLICKING NEXT ***");
-      await page.click(".Pagination__Button--next");
-    }
-  }
-
-  fs.writeFile(
-    "espnADP.json",
-    JSON.stringify({
-      players,
-    }),
-    (err) => {
-      if (err) throw err;
-    }
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  // navigate to login page
+  await page.click(
+    "xpath/html/body/div[2]/header/div[2]/div/div[3]/ul/li[4]/a/span"
   );
-
-  await browser.close();
+  // await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+  // enter login credentials and click login
+  await page.type("#app_login_username", "username");
+  await page.type("#app_login_password", "password");
+  // await page.click("#app_login > div:nth-child(10) > button");
+  // await browser.close();
 })();
+
+// scrape code from chrome extension
+
+//Content script for reading league settings from CBS Rules page
+// var site = "CBS";
+// var siteid = window.location.host.split(".")[0];
+// var siteContent = {
+//     "site": site, "siteid": siteid, "type": "settings",
+//     "settings": {
+//         "map": {},
+//         "mapped": {},
+//         "includes": { "rules": true },
+//         "siteneeds": { "rules": true, "teams": true, "draftorder": true, "myteam": true }
+//     },
+//     "status": false
+// };
+// var map = {};
+// var mapped = {};
+
+// window.onload = function () {
+//     setTimeout(update, 3000);
+// };
+
+// function update() {
+//     //read html content
+//     readContent().then(function (result) {
+//         if (result === 'success') {
+//             //save to chrome storage sync
+//             save();
+//         }
+//     }, function (reject) {//error
+//         console.log(reject);
+//     });
+// }
+// function readContent() {
+//     return new Promise(function (resolve, reject) {
+//         var elements = document.getElementsByClassName('featureComponentContainer');
+//         if (elements !== undefined && elements.length > 0) {
+//             readSection(elements).then(function (result) {
+//                 if (result === 'success') {
+//                     siteContent.settings.map = map;
+//                     siteContent.settings.mapped = mapped;
+//                     siteContent.status = true;
+//                     resolve('success');
+//                 }
+//             });
+//         } else {
+//             setTimeout(update, 500);
+//         }
+//     });
+// }
+// function readSection(elements) {
+//     var element = null;
+//     return new Promise(function (resolve, reject) {
+//         var sections = ['LEAGUE IDENTITY', 'ROSTER LIMITS', 'SCORING SYSTEM', 'DRAFT SETTINGS'];
+//         for (var i = 0; i < elements.length; i++) {
+//             element = elements[i].getElementsByClassName('leftUnderline')[0];
+//             if (sections.includes(element.innerText)) {
+//                 var fields = [], category = '';
+//                 var rows = elements[i].getElementsByTagName('tr');
+//                 //loop through rows of settings
+//                 for (var j = 0; j < rows.length; j++) {
+//                     if (JSON.stringify(map).length > 7000) break;
+//                     element = rows[j];
+//                     if (element.className !== 'label' && element.className !== 'subtitle') {
+//                         var rule = element.children[0].innerText;
+//                         switch (element.children.length) {
+//                             case 2:
+//                                 map[category + '|' + rule] = element.children[1].innerText == Number(element.children[1].innerText) ? Number(element.children[1].innerText) : element.children[1].innerText;
+//                                 break;
+//                             case 3:
+//                                 switch (rule) {
+//                                     case 'FG':
+//                                         var arr = element.children[2].innerText.split('\n');
+//                                         map[category + '|Field Goals (FG)'] = Number(arr[0].replace(' points', '').replace(' point', ''));
+//                                         if (arr.length > 1) {
+//                                             var range = '';
+//                                             for (var a = 1; a < arr.length; a++) {//bonus ranges
+//                                                 if (arr[a].indexOf('for a FG') > -1) {
+//                                                     range = arr[a].substring(arr[a].indexOf('for a FG') + 6);
+//                                                     points = arr[a].split(' ')[1].replace(' points', '').replace(' point', '');
+//                                                     map[category + '|' + range + ' (FG)'] = Number(points);
+//                                                 }
+//                                             }
+//                                         }
+//                                         break;
+//                                     case 'PaYd':
+//                                     case 'RuYd':
+//                                     case 'ReYd':
+//                                         arr = element.children[2].innerText.split(' ');
+//                                         if (arr.length === 9) {
+//                                             if (Number(element.children[2].innerText.split(' ')[7]) > 0) {
+//                                                 points = Number(element.children[2].innerText.split(' ')[3]) / Number(element.children[2].innerText.split(' ')[7])
+//                                             } else {
+//                                                 points = Number(element.children[2].innerText.split(' ')[3])
+//                                             }
+//                                         } else {
+//                                             points = element.children[0].innerText === 'PAYD' ? 0.10 : 0.04;
+//                                         }
+//                                         map[category + '|' + element.children[1].innerText + ' (' + rule + ')'] = Number(points);
+//                                         break;
+//                                     case 'PaTD':
+//                                     case 'RuTD':
+//                                     case 'ReTD':
+//                                         arr = element.children[2].innerText.split('\n');
+//                                         if (arr.length > 1) {
+//                                             points = arr[0].replace(' points', '').replace(' point', '');
+//                                             map[category + '|' + element.children[1].innerText + ' (' + rule + ')'] = Number(points);
+//                                             //bonus ranges
+//                                             var average = 0, rangeCount = 1;
+//                                             for (var range = 1; range < arr.length; range++) {
+//                                                 if (arr[range].split(' ').length > 2) {
+//                                                     points = arr[range].split(' ')[1];
+//                                                     average += Number(points) / rangeCount;
+//                                                     rangeCount++;
+//                                                 }
+//                                             }
+//                                             if (arr.length > 1) {
+//                                                 map[category + '|' + element.children[1].innerText + ' (' + rule + '50)'] = average;
+//                                             }
+//                                         } else {
+//                                             points = element.children[2].innerText.replace(' points', '').replace(' point', '');
+//                                             map[category + '|' + element.children[1].innerText.replace(/[&\/\\#+$~%'"*?<>{}]/g, '_') + ' (' + rule + ')'] = Number(points);
+//                                         }
+//                                         break;
+//                                     case 'PA':
+//                                     case 'YDS':
+//                                         arr = element.children[2].innerText.split('\n');
+//                                         range = '';
+//                                         for (var a = 0; a < arr.length; a++) {//ranges
+//                                             if (arr[a].indexOf(' = ') > -1) {
+//                                                 range = rule + '|' + arr[a].split(' = ')[0];
+//                                                 points = arr[a].split(' = ')[1].replace(' points', '').replace(' point','');
+//                                                 map[category + '|' + range + ' (' + rule + ')'] = Number(points);
+//                                             }
+//                                         }
+//                                         break;
+//                                     default:
+//                                         points = element.children[2].innerText.replace(' points', '').replace(' point', '');
+//                                         map[category + '|' + element.children[1].innerText.replace(/[&\/\\#+$~%'"*?<>{}]/g, '_') + ' (' + rule + ')'] = Number(points);
+//                                         break;
+//                                 }
+//                                 break;
+//                             case 4://rosters
+//                                 for (var k = 1; k < element.children.length; k++) {
+//                                     if (fields[k] !== '') {
+//                                         map[category + '|' + rule + '|' + fields[k]] = Number(element.children[k].innerText);
+//                                     }
+//                                 }
+//                                 break;
+
+//                         }
+//                     } else {
+//                         //field headers
+//                         fields = [];
+//                         for (k = 0; k < element.children.length; k++) {
+//                             fields.push(element.children[k].innerText);
+//                         }
+//                         category = fields[0];
+//                     }
+//                 }
+//             }
+//         }
+//         //other settings for CBS
+//         mapped["General.Abbrev"] = map["DESCRIPTION|League E-mail Address"].split("@")[0].substring(0, 8).toUpperCase();
+//         mapped["Rosters.Pos.BE.Bench"] = map["STATUS|Bench|MAX"];
+//         resolve('success');
+//     });
+// }
+// function save() {
+//     //save to chrome storage sync
+//     chrome.storage.sync.set({ "siteContent": siteContent }, function () {
+//         console.log(siteContent);
+//     });
+// }
+// //listen to changes to storage sync
+// chrome.storage.onChanged.addListener(function (changes, namespace) {
+//     console.log(changes);
+//     if (changes.import && changes.import.newValue) {
+//         var obj = changes.import.newValue;
+//         if (obj.site === site && obj.siteid === siteid && obj.sync === "settings") {
+//             console.log('call update');
+//             console.log(changes);
+//             update();
+//         }
+//     }
+// });
