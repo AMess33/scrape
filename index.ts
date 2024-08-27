75d2dc78029f0d80656e9b4baa67321753c1817f
Andrew M <amess3309@gmail.com>
2024-08-27 14:39:07 -0500

    fix selectors, add timeouts to slow down process, json files moved to json dir

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ let draftTypes = [
 
 const DLF_Rankings = async (draftTypes: { url: string; lable: string }) => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: false,
     slowMo: 10,
     executablePath: executablePath(),
@@ -53,7 +53,8 @@ const DLF_Rankings = async (draftTypes: { url: string; lable: string }) => {
   await page.click(
     "xpath/html/body/div[1]/section/div/div/div/section[2]/div/div[3]/div/div[2]/div/div/ul/li[2]/a"
   );
-
+  // process wasn't fulling typing before moving on to next steps, wait 5 seconds for login form load
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   await page.waitForSelector(
     "xpath/html/body/div[4]/div/div[2]/div/div/div/form/div[1]/input"
   );
@@ -71,18 +72,18 @@ const DLF_Rankings = async (draftTypes: { url: string; lable: string }) => {
   await page.click(
     "xpath/html/body/div[4]/div/div[2]/div/div/div/form/div[5]/input[1]"
   );
+  // allow login submit time to complete and page load
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   await page.goto(draftTypes.url, { waitUntil: "domcontentloaded" });
 
   await page.waitForSelector(
-    "xpath/html/body/div[1]/section/div/div/div/section[2]/div/div[3]/div/div[1]/div/nav[1]/ul/li[2]/a/img"
+    "table#avgTable > tbody > tr > td:nth-child(4) > a"
   );
-
-  await page.waitForSelector("table > tbody > tr > td:nth-child(4) > a");
 
   const rankingsData = await page.evaluate(() => {
     const playerRows = Array.from(
-      document.querySelectorAll("table > tbody > tr")
+      document.querySelectorAll("table#avgTable > tbody > tr")
     );
     // map each row in the table
     const data = playerRows.map((player: any) => ({
